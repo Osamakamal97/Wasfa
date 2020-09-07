68aa288ca4beb32bb98cefdfcccac930698ef2d9
OsamaKJ <osmaka1997@gmail.com>
2020-09-07 19:36:54 +0300

    solve faker factory error 0.1

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -3,9 +3,9 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Models\Category;
-use Faker\Generator as Faker;
+// use Faker\Generator;
 
-$factory->define(Category::class, function (Faker $faker) {
+$factory->define(Category::class, function (Faker\Generator $faker) {
     return [
         'name' => 'Category' . $faker->numberBetween(1, 100)
     ];
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,8 +3,7 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\User;
-use Faker\Generator as Faker;
-use Illuminate\Support\Facades\Hash;
+// use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 
 /*
@@ -17,8 +16,7 @@ use Illuminate\Support\Str;
 | model instances for testing / seeding your application's database.
 |
 */
-
-$factory->define(User::class, function (Faker $faker) {
+$factory->define(User::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
